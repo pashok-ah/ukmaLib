eb9ea21c007b1baf77873cd28b8ed037d3a6e3b2
pashok-ah <pashok.ah@gmail.com>
2016-05-21 21:56:14 +0300

    Remove books with the same name from Similar books list

--- app/spark/SubjectsSparkRecommender.scala
+++ app/spark/SubjectsSparkRecommender.scala
@@ -20,53 +20,75 @@ class SubjectsSparkRecommender @Inject()(val configuration: play.api.Configurati
   val numOfSimilarBooksToFind_ = configuration.getInt(
     "subjectsSparkRecommender.numberOfBooksToStore").getOrElse(5)
 
-  private def getIdAndSubjectsRdd(inputRdd: RDD[(Object, BSONObject)]): RDD[(Int, Array[String])] = {
+  private def getTitleIdAndSubjectsRdd(inputRdd: RDD[(Object, BSONObject)]):
+  RDD[(String, (Int, Array[String]))] = {
     val res = inputRdd.map(arg => {
       val arrFeatures = arg._2.get("subjects").asInstanceOf[BasicDBList].toArray
-      (arg._2.get("_id").asInstanceOf[Int], arrFeatures.map(_.toString))
+      (arg._2.get("bookTitle").asInstanceOf[String].toLowerCase, (arg._2.get("_id").asInstanceOf[Int],
+        arrFeatures.map(_.toString)))
     })
     res
   }
 
+  // groping id's by Title
+  def filter(inputRdd: RDD[(String, (Int, Array[String]))]): RDD[((Int, Array[Int]),Array[String])] = {
+    val distinctNamesRdd = inputRdd
+      .aggregateByKey(mutable.HashSet.empty[(Int, Array[String])])(addToSet, mergePartitionSets)
+      .map { case (bookTitle, hashSet) =>
+        val arrayOfIds = hashSet.toArray.map(_._1)
+        ((arrayOfIds.head, arrayOfIds.distinct),hashSet.head._2) }
+    println("Count after removing similar titles:"+distinctNamesRdd.count())
+    distinctNamesRdd
+  }
+
+  // using title as key coz of different editions of the same book
   def updateSimilarBooks(): Unit = {
-    val inputRDD = getIdAndSubjectsRdd(getCollectionFromMongoRdd(booksCollectionName_))
+    val inputRDD = filter(getTitleIdAndSubjectsRdd(getCollectionFromMongoRdd(booksCollectionName_)))
       .mapValues {
         case arrayFeatures => arrayFeatures.filter((feature) => feature != "Fiction")
       }
-    //getting (bookid,numSubjects),Array[String]
+
     val inputWithNumSubjectsRDD = inputRDD.map {
-      case (bookId, featuresArray) => ((bookId, featuresArray.size), featuresArray)
+      case ((bookId,idArray), featuresArray) => ((bookId, idArray, featuresArray.length), featuresArray)
     }
-    //got featureName, (bookid, numFeaturesForBook, numBooksWithFeature)
+
     val joinedWithFeatureCountsRdd = inputWithNumSubjectsRDD.flatMapValues(x => x)
       .map {
-        case ((bookId, numberOfFeatures), featureName) => (featureName, (bookId, numberOfFeatures))
+        case ((bookId, idArray, numberOfFeatures), featureName) => (featureName, (bookId, idArray, numberOfFeatures))
       }
-    //creating pairs with common features ((book1Id, book2Id),(numFeatures1Book, numFeatures2Book, numBoooksWithfeature)
+
     val bookPairsRDD = joinedWithFeatureCountsRdd.join(joinedWithFeatureCountsRdd)
       .filter(line => line._2._1._1 < line._2._2._1)
-      .map { case (featureName, ((bookId1, numberOfFeatures1),
-      (bookId2, numberOfFeatures2))) =>
-        ((bookId1, bookId2), (1, numberOfFeatures1, numberOfFeatures2))
+      .map { case (featureName, ((bookId1, idArray1, numberOfFeatures1),
+      (bookId2, idArray2, numberOfFeatures2))) =>
+        ((bookId1, bookId2), (1, idArray1, idArray2, numberOfFeatures1, numberOfFeatures2))
       }
     joinedWithFeatureCountsRdd.unpersist()
     // getting (book1Id,book2Id), Option[Correlation:Double]
     val joinedBooksRDD = bookPairsRDD
-      .reduceByKey((left, right) => (left._1 + right._1, left._2, left._3))
+      .reduceByKey((left, right) => (left._1 + right._1, left._2, left._3, left._4, left._5))
+        .map{case ((book1Id, book2Id),
+        (commonFeaturesNumber, idArray1, idArray2, numberOfFeatures1, numberOfFeatures2)) =>
+          ((book1Id, book2Id,idArray1, idArray2), (commonFeaturesNumber, numberOfFeatures1, numberOfFeatures2))
+        }
       .mapValues(value => calculateCorrelation(value))
     bookPairsRDD.unpersist()
     //getting bookid, Array[bookid]
-    val similarBooksIdsRdd = joinedBooksRDD.map { case ((book1Id, book2Id), optionCorrelation) =>
-      Array((book1Id, (book2Id, optionCorrelation)), (book2Id, (book1Id, optionCorrelation)))
+    val similarBooksIdsRdd = joinedBooksRDD.map {
+      case ((book1Id, book2Id, idArray1, idArray2), optionCorrelation) =>
+      Array((book1Id, (book2Id, optionCorrelation, idArray1)), (book2Id, (book1Id, optionCorrelation, idArray2)))
     }
       .flatMap(x => x)
-      .aggregateByKey(mutable.HashSet.empty[(Int, Option[Double])])(addToSet, mergePartitionSets)
+      .aggregateByKey(mutable.HashSet.empty[(Int, Option[Double], Array[Int])])(addToSet, mergePartitionSets)
       .map { case (bookId, similarBooksSet) =>
-        val arrayOfSimilarBooks = similarBooksSet.toArray.sortBy(_._2).reverse.map(line => line._1)
-        if (arrayOfSimilarBooks.length >= numOfSimilarBooksToFind_)
-          (bookId, arrayOfSimilarBooks.take(numOfSimilarBooksToFind_))
-        else (bookId, arrayOfSimilarBooks)
-      }
+        var arrayOfSimilarBooks = similarBooksSet.toArray.sortBy(_._2).reverse.map(line => line._1)
+        arrayOfSimilarBooks = if (arrayOfSimilarBooks.length > numOfSimilarBooksToFind_)
+          arrayOfSimilarBooks.take(numOfSimilarBooksToFind_) else arrayOfSimilarBooks
+        val fullArrayIdsOfBooksWithSameName = similarBooksSet.head._3.map{
+          case bookID => (bookID, arrayOfSimilarBooks)
+        }
+        fullArrayIdsOfBooksWithSameName
+      }.flatMap (x => x)
     joinedBooksRDD.unpersist()
     updateMongoCollectionWithRdd(booksCollectionName_, similarBooksIdsRdd.map { resTuple =>
       (new Object, getMongoUpdateWritableFromIdValueTuple[Int, Array[Int]](resTuple, "_id", "similarBooks"))
@@ -82,6 +104,5 @@ class SubjectsSparkRecommender @Inject()(val configuration: play.api.Configurati
     val numerator = numCommonFeatures
     val denominator = num1Features + num2Features
     Some(numerator.toDouble / denominator.toDouble)
-
   }
 }
